0a8dfbe555ca17c95943f321595b07a13e597566
Mohamed-Ziyad <ziyad.web.developer@gmail.com>
2019-03-31 13:44:10 +0530

    24% completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import TourList from './components/TouList';
 import './App.scss';
 
 class App extends Component {
 	render() {
-		return <Navbar />;
+		return (
+			<main>
+				<TourList />
+				<Navbar />
+			</main>
+		);
 	}
 }
 
---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './Tour.scss';
+
+class Tour extends Component {
+	render() {
+		return <div>Signle imge</div>;
+	}
+}
+
+export default Tour;
